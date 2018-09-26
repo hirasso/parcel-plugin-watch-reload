f3364237ee838456ad1255575454b50c25c91222
Rasso Hilber <mail@rassohilber.com>
2018-09-26 13:01:17 +0200

    Fix unit test

--- test/basic.js
+++ test/basic.js
@@ -9,7 +9,6 @@ fs.emptyDirSync( path.join(__dirname, './dist') )
 function test() {
   describe('Basic', function() {
     it('Should trigger a browser reload on file change', function(done) {
-      this.timeout( 5000 );
       const watchReload = new WatchReload( false );
       
       watchReload.on( 'reloadBrowsers', () => {
