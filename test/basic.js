5704001754e24273c07b582ce062a962889a8d67
Rasso Hilber <mail@rassohilber.com>
2018-09-26 13:34:47 +0200

    Standalone watcher

--- test/basic.js
+++ test/basic.js
@@ -6,7 +6,7 @@ const WatchReload = require('../lib/watch-reload');
 describe('Basic', function() {
   it('Should trigger a browser reload on file change', function(done) {
     this.timeout( 3000 );
-    const watchReload = new WatchReload( false );
+    const watchReload = new WatchReload();
 
     deleteTesterFile();    
 
